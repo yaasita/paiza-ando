68b3bfc8c3261c3312e1c599e11d05b4fdab0d37
yamasita <yamasita.taisuke@bpsinc.jp>
2015-12-14 00:17:04 +0900

    update

--- ando13.pl
+++ ando13.pl
@@ -1,18 +1,16 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use bigint;
 use feature qw(say);
 
 my $n = <STDIN>;
-my $str = factorial($n);
-$str =~ s/0+$//;
-$str =~ s/^.*(\d{9})$/$1/;
-$str =~ s/^0*//;
-say $str;
-
-sub factorial {
-    my ($n) = @_;
-    return ($n * &factorial($n -1)) if ($n > 0);
-    return 1;
+my $answer = 1;
+for (1..$n){
+    $answer *= $_;
+    $answer =~ s/0+$//;
+    $answer = substr($answer,-12);
 }
+
+$answer = substr($answer,-9);
+$answer =~ s/^0+//;
+say $answer;
